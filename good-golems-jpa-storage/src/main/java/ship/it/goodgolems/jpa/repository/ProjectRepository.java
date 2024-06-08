0c2a961e2026984dd1e44eb26b92f64fc6fd58bb
mariusz dolata <mariusz.dolata@gmail.com>
2024-06-08 22:57:49 +0200

    dodanie interfejsow crudRepository dla wszystkich encji

---
+++ good-golems-jpa-storage/src/main/java/ship/it/goodgolems/jpa/repository/ProjectRepository.java
@@ -0,0 +1,6 @@
+package ship.it.goodgolems.jpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<ProjectRepository, Long> {
+}

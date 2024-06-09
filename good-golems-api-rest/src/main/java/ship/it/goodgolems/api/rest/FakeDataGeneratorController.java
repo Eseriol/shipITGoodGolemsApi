12d14c8add20268e0357488311c1d330906d63ba
mariusz dolata <mariusz.dolata@gmail.com>
2024-06-09 14:08:39 +0200

    dodanie endpointow GET /api/v1/faker/employees oraz GET /api/v1/faker/projects

--- good-golems-api-rest/src/main/java/ship/it/goodgolems/api/rest/FakeDataGeneratorController.java
+++ good-golems-api-rest/src/main/java/ship/it/goodgolems/api/rest/FakeDataGeneratorController.java
@@ -3,16 +3,14 @@ package ship.it.goodgolems.api.rest;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import ship.it.goodgolems.api.FakeDataGeneratorApi;
 import ship.it.goodgolems.api.rest.presentation.Converter;
 import ship.it.goodgolems.api.rest.presentation.EmployeeDto;
 import ship.it.goodgolems.api.rest.presentation.ProjectDto;
+import ship.it.goodgolems.core.FakeDataGeneratorServiceImpl;
 
 
 @RestController
@@ -21,7 +19,7 @@ import ship.it.goodgolems.api.rest.presentation.ProjectDto;
 public class FakeDataGeneratorController {
 
     private final FakeDataGeneratorApi fakeDataGeneratorApi;
-
+    private final FakeDataGeneratorServiceImpl fakeDataGeneratorServiceImpl;
     @PostMapping("/employees")
     public ResponseEntity<List<EmployeeDto>> fakeEmployeesDataGen(@RequestParam int generationTarget) {
 
@@ -30,10 +28,27 @@ public class FakeDataGeneratorController {
                 .toList();
         return ResponseEntity.ok(employeeDtos);
     }
+    @GetMapping("/employees")
+    public ResponseEntity<List<EmployeeDto>> getAllEmployeesFromH2(){
+        List<EmployeeDto> employeeDtos = fakeDataGeneratorServiceImpl.getAllEmployees()
+                .stream()
+                .map(Converter::convert)
+                .toList();
+        return ResponseEntity.ok(employeeDtos);
+    }
 
     @PostMapping("/project")
     public ResponseEntity<List<ProjectDto>> fakeProjectDataGen(@RequestParam int generationTarget) {
         List<ProjectDto> projectDtos = fakeDataGeneratorApi.generateProjects(generationTarget).stream()
+                .map(Converter::convert)
+                .toList();
+        return ResponseEntity.ok(projectDtos);
+    }
+
+    @GetMapping("/projects")
+    public ResponseEntity<List<ProjectDto>> getAllProjectsFromH2(){
+        List<ProjectDto> projectDtos = fakeDataGeneratorServiceImpl.getAllProjects()
+                .stream()
                 .map(Converter::convert)
                 .toList();
         return ResponseEntity.ok(projectDtos);
--- good-golems-core/src/main/java/ship/it/goodgolems/core/FakeDataGeneratorServiceImpl.java
+++ good-golems-core/src/main/java/ship/it/goodgolems/core/FakeDataGeneratorServiceImpl.java
@@ -2,6 +2,7 @@ package ship.it.goodgolems.core;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ship.it.goodgolems.api.FakeDataGeneratorApi;
@@ -9,38 +10,25 @@ import ship.it.goodgolems.domain.Employee;
 import ship.it.goodgolems.domain.Project;
 import ship.it.goodgolems.jpa.mapper.EmployeeMapper;
 import ship.it.goodgolems.jpa.mapper.ProjectMapper;
-import ship.it.goodgolems.jpa.model.EmployeeEntity;
-import ship.it.goodgolems.jpa.model.ProjectEntity;
 import ship.it.goodgolems.jpa.repository.EmployeeRepository;
 import ship.it.goodgolems.jpa.repository.ProjectRepository;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 @Data
 public class FakeDataGeneratorServiceImpl {
     @Value("${application.autostart.fake-employee-number}")
     private int fakeEmployeeNumber;
     @Value("${application.autostart.fake-project-number}")
     private int fakeProjectNumber;
-    private List<Employee> fakeEmployees;
-    private List<EmployeeEntity> fakeEmployeesEntity;
-    private List<Project> fakeProjects;
-    private List<ProjectEntity> fakeProjectsEntity;
     private final EmployeeMapper employeeMapper;
     private final ProjectMapper projectMapper;
     private final EmployeeRepository employeeRepository;
     private final ProjectRepository projectRepository;
 
     private final FakeDataGeneratorApi fakeDataGeneratorApi;
-
-    public FakeDataGeneratorServiceImpl(FakeDataGeneratorApi fakeDataGeneratorApi, EmployeeMapper employeeMapper, ProjectMapper projectMapper, EmployeeRepository employeeRepository, ProjectRepository projectRepository){
-        this.fakeDataGeneratorApi=fakeDataGeneratorApi;
-        this.employeeMapper = employeeMapper;
-        this.projectMapper = projectMapper;
-        this.employeeRepository=employeeRepository;
-        this.projectRepository = projectRepository;
-    }
 
     @PostConstruct
     public void generateFakeEmployees(){
@@ -54,5 +42,19 @@ public class FakeDataGeneratorServiceImpl {
                 .stream()
                 .map(projectMapper::map)
                 .toList());
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll()
+                .stream()
+                .map(employeeMapper::map)
+                .toList();
+    }
+
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll()
+                .stream()
+                .map(projectMapper::map)
+                .toList();
     }
 }

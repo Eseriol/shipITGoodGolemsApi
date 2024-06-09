ece50afea37e7ba3518d5a6a8d4d0cf677d5cb95
mariusz dolata <mariusz.dolata@gmail.com>
2024-06-09 18:55:56 +0200

    wyrzucenie podwojnego employee z kazdego endpointu parametrowego

--- good-golems-api-rest/src/main/java/ship/it/goodgolems/api/rest/EmployeeController.java
+++ good-golems-api-rest/src/main/java/ship/it/goodgolems/api/rest/EmployeeController.java
@@ -31,57 +31,57 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeDtos);
     }
 
-    @GetMapping("/employees/by-technology")
+    @GetMapping("/by-technology")
     public List<EmployeeEntity> getEmployeesByTechnology(@RequestParam String technologyName) {
         return employeeService.getEmployeesByTechnology(technologyName);
     }
 
-    @GetMapping("/employees/by-position")
+    @GetMapping("/by-position")
     public List<EmployeeEntity> getEmployeesByPosition(@RequestParam String position) {
         return employeeService.getEmployeesByPosition(position);
     }
 
-    @GetMapping("/employees/by-project-name")
+    @GetMapping("/by-project-name")
     public List<EmployeeEntity> getEmployeesByProjectName(@RequestParam String projectName) {
         return employeeService.getEmployeesByProjectName(projectName);
     }
 
-    @GetMapping("/employees/by-technology-and-min-years")
+    @GetMapping("/by-technology-and-min-years")
     public List<EmployeeEntity> getEmployeesByTechnologyAndMinYears(@RequestParam String technologyName, @RequestParam int years) {
         return employeeService.getEmployeesByTechnologyAndMinYears(technologyName, years);
     }
 
-    @GetMapping("/employees/by-project-id")
+    @GetMapping("/by-project-id")
     public List<EmployeeEntity> getEmployeesByProjectId(@RequestParam Long projectId) {
         return employeeService.getEmployeesByProjectId(projectId);
     }
 
-    @GetMapping("/employees/by-full-name")
+    @GetMapping("/by-full-name")
     public List<EmployeeEntity> getEmployeesByFullName(@RequestParam String fullName) {
         return employeeService.getEmployeesByFullName(fullName);
     }
 
-    @GetMapping("/employees/by-technology-names")
+    @GetMapping("/by-technology-names")
     public List<EmployeeEntity> getEmployeesByTechnologyNames(@RequestParam List<String> technologyNames) {
         return employeeService.getEmployeesByTechnologyNames(technologyNames);
     }
 
-    @GetMapping("/employees/by-project-requirement")
+    @GetMapping("/by-project-requirement")
     public List<EmployeeEntity> getEmployeesByProjectRequirement(@RequestParam String technologyName) {
         return employeeService.getEmployeesByProjectRequirement(technologyName);
     }
 
-    @GetMapping("/employees/by-seniority-level")
+    @GetMapping("/by-seniority-level")
     public List<EmployeeEntity> getEmployeesBySeniorityLevel(@RequestParam String seniorityLevel) {
         return employeeService.getEmployeesBySeniorityLevel(seniorityLevel);
     }
 
-    @GetMapping("/employees/by-stack-and-min-years")
+    @GetMapping("/by-stack-and-min-years")
     public List<EmployeeEntity> getEmployeesByStackAndMinYears(@RequestParam String technologyName, @RequestParam int years) {
         return employeeService.getEmployeesByStackAndMinYears(technologyName, years);
     }
 
-    @GetMapping("/employees/by-number-of-projects-greater-than")
+    @GetMapping("/by-number-of-projects-greater-than")
     public List<EmployeeEntity> getEmployeesByNumberOfProjectsGreaterThan(@RequestParam int numberOfProjects) {
         return employeeService.getEmployeesByNumberOfProjectsGreaterThan(numberOfProjects);
     }
